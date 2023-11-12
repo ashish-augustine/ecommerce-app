07ac94693f655e4e979d1a1f06f258f6035e610d
Ashish Augustine <ashish.augustine123@gmail.com>
2023-11-12 11:51:38 +0100

    commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
-import { Footer } from "."
-import {Nav} from "."
+import { Footer } from ".";
+import { Nav } from ".";
 import React from "react";
-import AuthWrapper from './AuthWrapper'
+import AuthWrapper from "./AuthWrapper";
 // import ReactDOM from "react-dom";
 import { ProductsProvider } from "../context/products_context";
 import { FilterProvider } from "../context/filter_context";
@@ -9,31 +9,28 @@ import { CartProvider } from "../context/cart_context";
 import { UserProvider } from "../context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-export default function Layout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    return <>
-    <Auth0Provider
-    domain="dev-74yaitxhvcrqnokk.us.auth0.com"
-    clientId="IMOQb4ZqbVtFUfSbTC0BAP0GgDjhAcNL"
-    redirectUri={window.location.origin}
-    cacheLocation="localstorage">
-    <UserProvider>
-      <ProductsProvider>
-        <FilterProvider>
-          <CartProvider>
-            <AuthWrapper>
-              <Nav/>
-              {children}
-              <Footer/>
-            </AuthWrapper>
-          </CartProvider>
-        </FilterProvider>
-      </ProductsProvider>
-    </UserProvider>
-  </Auth0Provider>
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Auth0Provider
+        domain="dev-74yaitxhvcrqnokk.us.auth0.com"
+        clientId="foBK6yAWSNoFZKNvxtmG6Ns8jcD14E45"
+        redirectUri={window.location.origin}
+        cacheLocation="localstorage">
+        <UserProvider>
+          <ProductsProvider>
+            <FilterProvider>
+              <CartProvider>
+                <AuthWrapper>
+                  <Nav />
+                  {children}
+                  <Footer />
+                </AuthWrapper>
+              </CartProvider>
+            </FilterProvider>
+          </ProductsProvider>
+        </UserProvider>
+      </Auth0Provider>
     </>
-
+  );
 }
